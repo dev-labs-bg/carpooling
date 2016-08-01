35fa64051a9e66882ccba2d84f062e4a4f707eb6
Ivo Karagyozov <ivokaragyozov@hotmail.com>
2016-08-01 10:37:35 +0300

    Add comments to the users api and repository

--- app/api/users.js
+++ app/api/users.js
@@ -1,17 +1,27 @@
 var bodyParser = require( 'body-parser' );
 var userRepository = require( '../repositories/user.js' );
 
+/**
+ *  This is the main part in the user api
+ *
+ *  From here all methods connected with users will be executed
+ *
+ * @param app
+ */
 module.exports = function( app ) {
   app.use( bodyParser.json() );
   app.use( bodyParser.urlencoded( {extended: true} ) );
 
+  // When a new user needs to be registered
   app.post( '/api/users', function( req, res ) {
+
+    //Get the user parameters from the request's body and execute the create user method from
+    // the user repository
     var userParams = req.body;
     userRepository.createUser( userParams )
       .then( function( product ) {
         res.json( product );
       } ).catch( function( err ) {
-      console.log( 'inside' );
       res.json( err );
     } );
   } );
--- app/repositories/user.js
+++ app/repositories/user.js
@@ -1,7 +1,17 @@
 var mongoose = require( 'mongoose' );
 var User = require( '../models/user.js' );
 
+/**
+ * The create user method
+ *
+ * It is used by users api to register new user
+ *
+ * @param userParams
+ * @returns {*}
+ */
 module.exports.createUser = function( userParams ) {
+
+  // Create new user with the given params ans saves it to the database
   var newUser = new User( userParams );
   return newUser.save();
 };
